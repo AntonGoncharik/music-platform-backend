4a32c811160c41e1745b7f6883080747268fbd8a
AntonGoncharik <ant.goncharik@gmail.com>
2021-08-13 02:01:17 +0300

    added pipe

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import * as helmet from 'helmet';
 
 import { AppModule } from './app.module';
+import { ValidationPipe } from './pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString({ message: 'Should be string' })
+  @IsEmail({}, { message: 'Incorrect email' })
   email: string;
+  @IsString({ message: 'Should be string' })
+  @Length(4, 16, { message: 'Not less than 4 and not more than 16 characters' })
   password: string;
 }
